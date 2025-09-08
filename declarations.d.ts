40cc2a3e24aae699ccb0a3abd3d96ae53f6bc444
shaikmohammedamaan245-sketch <shaikmohammedamaan245@gmail.com>
2025-09-08 15:14:12 +0530

    Update declarations.d.ts

--- declarations.d.ts
+++ declarations.d.ts
@@ -1,10 +1,1 @@
-declare module "pg" {
-  import { EventEmitter } from "events";
 
-  export class Pool extends EventEmitter {
-    constructor(config?: any);
-    connect(): Promise<any>;
-    query(queryText: string, values?: any[]): Promise<any>;
-    end(): Promise<void>;
-  }
-}
